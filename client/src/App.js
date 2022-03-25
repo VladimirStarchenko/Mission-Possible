bb8c0d06a0009d0d1390a3bd0bb61fb2e0c6b55d
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2022-03-24 23:24:02 -0400

    get rid of un-needed comments and extra .gitignore

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import { ApolloProvider } from "@apollo/react-hooks";
-// import ApolloClient from "apollo-boost";
-// import SearchBooks from './pages/SearchBooks';
 import SavedCharities from "./pages/SavedCharities";
 import Navbar from "./components/Navbar";
 import StripePayment from "./components/StripePayment";
 
 import { useQuery } from "@apollo/client";
 import { GET_CHARITIES } from "./utils/queries.js";
-
-// const client = new ApolloClient({
-//   request: (operation) => {
-//     const token = localStorage.getItem("id_token");
-
-//     operation.setContext({
-//       headers: {
-//         authorization: token ? `Bearer ${token}` : "",
-//       },
-//     });
-//   },
-//   uri: "/graphql",
-// });
-// send graphQL operations to server
 
 function App() {
   /**------------------------------

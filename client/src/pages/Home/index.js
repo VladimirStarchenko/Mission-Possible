578bc8a1627a1c43eb50694be106392a38b740f7
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2022-03-28 23:29:05 -0400

    add projects carousel

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -274,6 +274,130 @@ function Home() {
       </div>
       {/* <!-- Service End --> */}
 
+      {/* <!-- Causes Start --> */}
+      <div className="causes">
+        <div className="container">
+          <div className="section-header text-center">
+            <p>Popular Causes</p>
+            <h2>Let's know about charity causes around the world</h2>
+          </div>
+          <div className="owl-carousel causes-carousel">
+            <div className="causes-item">
+              <div className="causes-img">
+                <img src="/assets/img/causes-1.jpg" alt="Image" />
+              </div>
+              <div className="causes-progress">
+                <div className="progress">
+                  <div
+                    className="progress-bar"
+                    role="progressbar"
+                    aria-valuenow="0"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  >
+                    <span>0%</span>
+                  </div>
+                </div>
+                <div className="progress-text">
+                  <p>
+                    <strong>Raised:</strong> $0
+                  </p>
+                  <p>
+                    <strong>Goal:</strong> $50000
+                  </p>
+                </div>
+              </div>
+              <div className="causes-text">
+                <h3>Lorem ipsum dolor sit</h3>
+                <p>
+                  Lorem ipsum dolor sit amet elit. Phasell nec pretium mi.
+                  Curabit facilis ornare velit non vulputa
+                </p>
+              </div>
+              <div className="causes-btn">
+                <a className="btn btn-custom">Learn More</a>
+                <a className="btn btn-custom">Donate Now</a>
+              </div>
+            </div>
+            <div className="causes-item">
+              <div className="causes-img">
+                <img src="/assets/img/causes-2.jpg" alt="Image" />
+              </div>
+              <div className="causes-progress">
+                <div className="progress">
+                  <div
+                    className="progress-bar"
+                    role="progressbar"
+                    aria-valuenow="0"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  >
+                    <span>0%</span>
+                  </div>
+                </div>
+                <div className="progress-text">
+                  <p>
+                    <strong>Raised:</strong> $0
+                  </p>
+                  <p>
+                    <strong>Goal:</strong> $50000
+                  </p>
+                </div>
+              </div>
+              <div className="causes-text">
+                <h3>Lorem ipsum dolor sit</h3>
+                <p>
+                  Lorem ipsum dolor sit amet elit. Phasell nec pretium mi.
+                  Curabit facilis ornare velit non vulputa
+                </p>
+              </div>
+              <div className="causes-btn">
+                <a className="btn btn-custom">Learn More</a>
+                <a className="btn btn-custom">Donate Now</a>
+              </div>
+            </div>
+            <div className="causes-item">
+              <div className="causes-img">
+                <img src="/assets/img/causes-3.jpg" alt="Image" />
+              </div>
+              <div className="causes-progress">
+                <div className="progress">
+                  <div
+                    className="progress-bar"
+                    role="progressbar"
+                    aria-valuenow="0"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  >
+                    <span>0%</span>
+                  </div>
+                </div>
+                <div className="progress-text">
+                  <p>
+                    <strong>Raised:</strong> $0
+                  </p>
+                  <p>
+                    <strong>Goal:</strong> $50000
+                  </p>
+                </div>
+              </div>
+              <div className="causes-text">
+                <h3>Lorem ipsum dolor sit</h3>
+                <p>
+                  Lorem ipsum dolor sit amet elit. Phasell nec pretium mi.
+                  Curabit facilis ornare velit non vulputa
+                </p>
+              </div>
+              <div className="causes-btn">
+                <a className="btn btn-custom">Learn More</a>
+                <a className="btn btn-custom">Donate Now</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <!-- Causes End --> */}
+
       {/* <!-- Donate Start --> */}
       <div
         className="donate"

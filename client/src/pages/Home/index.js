0118c987425c3b33b4f49180598d9c16054ebe68
Fabulosa <fabiscg79@gmail.com>
2022-03-29 16:19:04 -0400

    fixed some images sizing and padding

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -29,11 +29,11 @@ function Home() {
 
       {/* <!-- Carousel Start --> */}
 
-      
-    
+
+
       {/* <img src="/assets/img/donateBox.jpg" alt="donate box" />  */}
 
-    
+
       {/* <!-- Carousel End --> */}
 
 
@@ -47,7 +47,7 @@ function Home() {
                 className="about-img"
                 data-parallax="scroll"
                 // data-image-src="/assets/img/facts.jpg"
-                data-image-src='/assets/img/donateBox.jpg'
+                data-image-src='/assets/img/happy-kids.jpg'
               ></div>
             </div>
             <div className="col-lg-6">
@@ -58,7 +58,7 @@ function Home() {
               <div className="about-tab">
                 <ul className="nav nav-pills nav-justified">
                   <li className="nav-item">
-                    <a id="vision-mission" 
+                    <a id="vision-mission"
                       className="nav-link active"
                       data-toggle="pill"
                       href="#tab-content-1"
@@ -67,7 +67,7 @@ function Home() {
                     </a>
                   </li>
                   <li className="nav-item">
-                    <a id="about-mission" 
+                    <a id="about-mission"
                       className="nav-link"
                       data-toggle="pill"
                       href="#tab-content-2"
@@ -76,8 +76,8 @@ function Home() {
                     </a>
                   </li>
                   <li className="nav-item">
-                    <a id="vision-tab" 
-                    style={{color:"inherit"}}
+                    <a id="vision-tab"
+                      style={{ color: "inherit" }}
                       className="nav-link"
                       data-toggle="pill"
                       href="#tab-content-3"
@@ -89,14 +89,14 @@ function Home() {
 
                 <div className="tab-content">
                   <div id="tab-content-1" className="container tab-pane active">
-                  We are a non profit organization working to raise money for various causes throughtout Canada. Benefactors can donate to the charity of their choice. 
+                    We are a non profit organization working to raise money for various causes throughtout Canada. Benefactors can donate to the charity of their choice.
                   </div>
                   <div id="tab-content-2" className="container tab-pane fade">
-                  Our mission to take make things possible through donations. 
+                    Our mission to take make things possible through donations.
 
                   </div>
                   <div id="tab-content-3" className="container tab-pane fade">
-                  Create change through donations! 
+                    Create change through donations!
                   </div>
                 </div>
               </div>
@@ -123,7 +123,7 @@ function Home() {
                 <div className="service-text">
                   <h3 id="democracy-header">DEMOCRACY NOW!</h3>
                   <p>
-                    A media outlet that combines news reporting, interviews, investigative journalism and political commentary, with a focus on peace activistism linked to environmental justice and social justice, guided by the ethics of ecofeminism as a philosophy. Democracy Now! gives activists and citizens a platform to debate the system. 
+                    A media outlet that combines news reporting, interviews, investigative journalism and political commentary, with a focus on peace activistism linked to environmental justice and social justice, guided by the ethics of ecofeminism as a philosophy. Democracy Now! gives activists and citizens a platform to debate the system.
                   </p>
                 </div>
               </div>
@@ -136,7 +136,7 @@ function Home() {
                 <div className="service-text">
                   <h3 id="comic-header">COMIC RELIEF</h3>
                   <p>
-                  Comic Relief invests in nonprofit and community-led organizations with programs focused on tackling the root causes and consequences of poverty and social injustice. We support initiatives and policies that advance economic opportunity and leadership development in communities directly impacted by intergenerational poverty.
+                    Comic Relief invests in nonprofit and community-led organizations with programs focused on tackling the root causes and consequences of poverty and social injustice. We support initiatives and policies that advance economic opportunity and leadership development in communities directly impacted by intergenerational poverty.
                   </p>
                 </div>
               </div>
@@ -149,7 +149,7 @@ function Home() {
                 <div className="service-text">
                   <h3 id="safe-header">SAFE PASSSSGE</h3>
                   <p>
-                  Every year thousands of child refugees arrive in Europe, almost half of them are unaccompanied and as a result are at risk of being abused and trafficked. Yet many of these children have a legal right to travel safely to a place of sanctuary. With our support they can access their rights. 
+                    Every year thousands of child refugees arrive in Europe, almost half of them are unaccompanied and as a result are at risk of being abused and trafficked. Yet many of these children have a legal right to travel safely to a place of sanctuary. With our support they can access their rights.
                   </p>
                 </div>
               </div>
@@ -163,7 +163,7 @@ function Home() {
       <div
         className="donate"
         data-parallax="scroll"
-        // data-image-src="/assets/img/donate.jpg"
+      // data-image-src="/assets/img/donate.jpg"
       >
         <div className="container">
           <div className="row align-items-center">
@@ -172,7 +172,7 @@ function Home() {
                 <div className="section-header">
                   <p id="donate-subheader">Donate Now</p>
                   <h2 id=
-                  "donate-header">Let's give to a cause to create a better world</h2>
+                    "donate-header">Let's give to a cause to create a better world</h2>
                 </div>
                 <div className="donate-text">
                   <p id="mission-statement">
@@ -227,20 +227,20 @@ function Home() {
       </div>
       {/* <!-- Donate End --> */}
 
-      
 
-    {/* <!-- Team Start --> */}
-  <div className="team">
+
+      {/* <!-- Team Start --> */}
+      <div className="team">
         <div className="container">
           <div className="section-header text-center">
             <span></span>
             <h2 id="founders-header">The founders of Mission Possible</h2>
           </div>
           <div className="row">
-            <div className="col-lg-3 col-md-6">
+            <div className="col-lg-4 col-md-6">
               <div className="team-item">
                 <div className="team-img">
-                <img src="/assets/img/arslan_razi.jpg" alt="Team" />
+                  <img className="avatar" src="/assets/img/arslan_razi.jpg" alt="Team" />
                 </div>
                 <div className="team-text">
                   <h2>Arslan Razi</h2>
@@ -255,10 +255,10 @@ function Home() {
                 </div>
               </div>
             </div>
-            <div className="col-lg-3 col-md-6">
+            <div className="col-lg-4 col-md-6">
               <div className="team-item">
                 <div className="team-img">
-                <img src="/assets/img/about-me.jpg" alt="Team" />
+                  <img className="avatar" src="/assets/img/about-me.jpg" alt="Team" />
                 </div>
                 <div className="team-text">
                   <h2>Bianca Chami</h2>
@@ -273,10 +273,10 @@ function Home() {
                 </div>
               </div>
             </div>
-            <div className="col-lg-3 col-md-6">
+            <div className="col-lg-4 col-md-6">
               <div className="team-item">
                 <div className="team-img">
-                <img src="/assets/img/daniel-az.jpeg" alt="Team" />
+                  <img src="/assets/img/daniel-az.jpeg" className="avatar" alt="Team" />
                 </div>
                 <div className="team-text">
                   <h2>Daniel Arzanipour</h2>
@@ -291,10 +291,10 @@ function Home() {
                 </div>
               </div>
             </div>
-            <div className="col-lg-3 col-md-6">
+            <div className="col-lg-4 col-md-6">
               <div className="team-item">
                 <div className="team-img">
-                  <img src="/assets/img/fabi-team.jpg" alt="Team" />
+                  <img src="/assets/img/my-avatar.jpg" className="avatar" alt="Team" />
                 </div>
                 <div className="team-text">
                   <h2>Fabiola Castaneda G.</h2>
@@ -310,10 +310,10 @@ function Home() {
               </div>
             </div>
 
-            <div className="col-lg-3 col-md-6">
+            <div className="col-lg-4 col-md-6">
               <div className="team-item">
                 <div className="team-img">
-                  <img src="/assets/img/joona-lu.jpg" alt="Team" />
+                  <img src="/assets/img/joona-lu.jpg" className="avatar" alt="Team" />
                 </div>
                 <div className="team-text">
                   <h2>Joona Lusa</h2>
@@ -329,10 +329,10 @@ function Home() {
               </div>
             </div>
 
-            <div className="col-lg-3 col-md-6">
+            <div className="col-lg-4 col-md-6">
               <div className="team-item">
                 <div className="team-img">
-                  <img src="/assets/img/vlad-avatar.jpg" alt="Team" />
+                  <img src="/assets/img/vlad-avatar.jpg" className="avatar" alt="Team" />
                 </div>
                 <div className="team-text">
                   <h2>Vladimir Starchenko</h2>
@@ -351,7 +351,7 @@ function Home() {
         </div>
       </div>
       {/* <!-- Team End --> */}
-      
+
       {/* <!-- Contact Start --> */}
       <div className="contact">
         <div className="container">
@@ -360,7 +360,7 @@ function Home() {
             <h2 id="contact-subheader">Contact Us!</h2>
           </div>
           <div className="contact-img">
-            <img src="/assets/img/contact.jpg" alt="contact" />
+            <img src="/assets/img/boxed-water.jpg" alt="contact" />
           </div>
           <div className="contact-form">
             <div id="success"></div>
@@ -409,7 +409,7 @@ function Home() {
                 <p className="help-block text-danger"></p>
               </div>
               <div>
-                <button 
+                <button
                   className="btn btn-block"
                   type="submit"
                   id="sendMessageButton"
@@ -421,7 +421,7 @@ function Home() {
           </div>
         </div>
       </div>
-      {/* <!-- Contact End --> */}     
+      {/* <!-- Contact End --> */}
       <Footer />
     </>
   );

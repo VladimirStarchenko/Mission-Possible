73656e940222213faab076c6c3ca2f51ddf02183
Fabulosa <fabiscg79@gmail.com>
2022-03-29 22:12:01 -0400

    fit images, added images from the charities, fixing navbar padding and deleting old images

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/missionpossible', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/missionpossible', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
